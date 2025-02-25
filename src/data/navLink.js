b8ab6d3fb071253cee0073bc1ee01cf0ef93f8a3
fiqhi007 <71620197+fiqhi007@users.noreply.github.com>
2025-02-25 08:47:23 +0700

    updating

---
+++ src/data/navLink.js
@@ -0,0 +1,10 @@
+// src/data/navLinks.js
+const navLinks = [
+    { text: "Home", url: "/" },
+    { text: "Element", url: "/element" },
+    { text: "Filer", url: "/filer" },
+    { text: "Contact", url: "/contact" },
+  ];
+  
+  export default navLinks;
+  
